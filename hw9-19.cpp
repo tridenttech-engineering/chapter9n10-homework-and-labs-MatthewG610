8a2008ad5e0a86b18f01c2ace0653ea1bf697864
MatthewG610 <mgodoy0101@gmail.com>
2025-03-25 17:37:34 -0400

    Update hw9-19.cpp

--- hw9-19.cpp
+++ hw9-19.cpp
@@ -1,5 +1,4 @@
-//Lab 9-2.cpp - displays two monthly car payments
-//Created/revised by <your name> on <current date>
+
 
 #include <iostream>
 #include <cmath>
@@ -8,6 +7,7 @@ using namespace std;
 
 //function prototype
 double getPayment(int, double, int);
+double getTotal(double monthPay, int months);
 
 int main()
 {
@@ -19,6 +19,8 @@ int main()
     int term = 0;
     double creditPayment = 0.0;
     double dealerPayment = 0.0;
+    double totalCredAmount = 0.0;
+    double totalDealerAmount = 0.0;
 
     cout << "Car price (after any trade-in): ";
     cin >> carPrice;
@@ -36,14 +38,19 @@ int main()
         creditRate / 12, term * 12);
     dealerPayment = getPayment(carPrice, 
         dealerRate / 12, term * 12);    //assign values to calculate payments
-    
+    totalCredAmount = getTotal(creditPayment, term * 12);
+    totalDealerAmount = getTotal(dealerPayment, term * 12);
     //display payments
     cout << fixed << setprecision(2) << endl; 
-    cout << "Credit union payment: $" 
-        << creditPayment << endl;
-    cout << "Dealer payment: $"
-        << dealerPayment << endl;
-    
+
+    cout << "Credit union payment: $" << creditPayment << endl;
+
+    cout<< "Total amount with credit union: $" << totalCredAmount << endl;
+
+    cout << "Dealer payment: $" << dealerPayment << endl;
+
+    cout<< "Total amount with dealer: $" << totalDealerAmount << endl;
+
     return 0;
 }//end of main function    
 
@@ -54,7 +61,22 @@ double getPayment(int prin,
 {
     //calculates and returns a monthly payment
     double monthPay = 0.0;
-    monthPay = prin * monthRate / 
-        (1 - pow(monthRate + 1, -months));
+
+    if(1 - pow(monthRate + 1, -months) == 0)
+        monthPay = -1;
+    else
+        monthPay = prin * monthRate / (1 - pow(monthRate + 1, -months));
     return monthPay;
+
 } //end of getPayment function//*****function definition*****
+
+    double getTotal(double monthPay, int months)
+{
+    cout << fixed << setprecision(2) << endl; 
+    double roundedPay = 0.0;
+    double total = 0.0;
+    roundedPay = static_cast<int>((monthPay + .005) * 100) / 100.0;   //Calculates the total amount that will be payed
+    total = roundedPay * months;
+    return total;
+
+}  //end of getTotal function
